500bc0bf255b4541a37d81589564fbf3521d17cd
bahautdinov_bahtiyar <sabakilai@gmail.com>
2017-07-15 13:28:12 +0600

    init

--- libs/create.js
+++ libs/create.js
@@ -4,9 +4,9 @@ var dict = require('./dict');
 
 module.exports = function () {
   var parsers = [];
-  for (var i = 1; i < 31; i++) {
-    parsers.push(parser(dict(i).branch,1));
-  }
+  //for (var i = 1; i < 31; i++) {
+    parsers.push(parser(dict(11).branch,1));
+  //}
   Promise.all(parsers).then((data)=>{
     console.log(data);
 
