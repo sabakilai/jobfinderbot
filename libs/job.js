56b8d38ee663077ca1ca4078e939751e4b556510
bahautdinov_bahtiyar <sabakilai@gmail.com>
2017-07-15 17:47:43 +0600

    init

--- libs/job.js
+++ libs/job.js
@@ -8,23 +8,30 @@ var dict = require('./dict');
 module.exports = function () {
   return new Promise ((resolve, reject)=>{
     var files = [];
-    var parsers = []
+    var parsers = [];
+    var tosend = [];
+    var saves = [];
     for (var i = 1; i < 31; i++) {
       files.push(AWS.read('jobkg' + dict(i.toString()).branch + '.json'));
     }
     Promise.all(files).then((file_link)=>{
-      console.log('file');
-      console.log(file_link[0].link);
-
       for (var i = 1; i < 31; i++) {
         parsers.push(parser(dict(i.toString()).branch,1));
       }
       Promise.all(parsers).then((parser_link)=>{
-        console.log('pareser');
-        console.log(parser_link[0][0].link);
-        resolve('hi!')
-
-
+        for (var i = 0; i < 30; i++) {
+          if (file_link.link[i]!=parser_link[i][0].link) {
+            console.log('new parser - ' + JSON.stringify(parser_link[0][0]));
+            AWS.save('jobkg' + dict((i+1).toString()).branch + '.json', JSON.stringify(parser_link[0][0])).then((message)=>{
+              console.log(message);
+            }).catch((error)=>{
+              console.log(error);;
+            })
+            tosend.push('jobkg' + dict((i+1).toString()).branch + '.json')
+          }
+        }
+        resolve(tosend);
+        
       }).catch((error)=>{
         reject('Error parsing: ' + error);
       })
