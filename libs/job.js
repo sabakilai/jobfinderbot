86b78a039ff217d94806e92e0c720b1651da52b6
bahautdinov_bahtiyar <sabakilai@gmail.com>
2017-07-15 17:24:23 +0600

    init

--- libs/job.js
+++ libs/job.js
@@ -13,10 +13,10 @@ module.exports = function () {
       files.push(AWS.read('jobkg' + dict(i.toString()).branch + '.json'));
     }
     Promise.all(files).then((file_link)=>{
-
+      console.log(file_link[0]);
 
       for (var i = 1; i < 31; i++) {
-        parsers.push(parser(dict(i).branch,1));
+        parsers.push(parser(dict(i.toString()).branch,1));
       }
       Promise.all(parsers).then((parser_link)=>{
 
