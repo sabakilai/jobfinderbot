a3f74b745d6fd5cb0b32af5141e323e576ca97c0
bahautdinov_bahtiyar <sabakilai@gmail.com>
2017-07-15 13:45:18 +0600

    init

--- libs/create.js
+++ libs/create.js
@@ -4,11 +4,20 @@ var dict = require('./dict');
 
 module.exports = function () {
   var parsers = [];
+  var saves = [];
   for (var i = 1; i < 31; i++) {
     parsers.push(parser(dict(i.toString()).branch,1));
   }
   Promise.all(parsers).then((data)=>{
-    console.log(data[0][0]);
-    console.log(data[1][0]);
+    for (var i = 0; i < 30; i++) {
+      saves.push(AWS.save('jobkg' + dict((i+1).toString()).branch + '.json',data[i][0]))
+    }
+    Promise.all(saves).then((messages)=>{
+      console.log(messages);
+    }).catch((error)=>{
+      console.log(error);
+    })
+  }).catch((error)=>{
+    console.log(error);
   })
 }
