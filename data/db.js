05f1057b4d080243b2bd2d2256cab1c879cd2a49
bahautdinov_bahtiyar <sabakilai@gmail.com>
2017-08-17 12:11:30 +0600

    init

--- data/db.js
+++ data/db.js
@@ -1,7 +1,10 @@
 var Sequelize = require("sequelize");
 var sequelize = new Sequelize("d9m253htprsfdd", "nmfbjytoecuyvd", "43517f5cfc92eb60958766f89afbd4f234c444aedd0ba88106cb9c061081cf76", {
 	host: "ec2-23-21-158-253.compute-1.amazonaws.com",
-	dialect: "postgres"
+	dialect: "postgres",
+  dialectOptions: {
+    ssl: true
+  }
 });
 
 var user = sequelize.define("user", {
